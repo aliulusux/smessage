9d13c4e9b4079deb637914c064b54ca3353d6ea7
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-10-22 11:38:43 +0300

    main

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../lib/supabase";
 import GlassAlert from "../components/GlassAlert";
 
 export default function Join() {
