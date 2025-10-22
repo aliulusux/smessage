a3812a2da4f29e6c07cce0fbed1512ffebb9e924
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-10-22 11:51:32 +0300

    main

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../lib/supabase";
 
 export default function Join() {
   const [username, setUsername] = useState("");
